a002f706a91d2edbfd59c1f1006aa5ee4b30c6a1
Daniel Nelga <g00464658@atu.ie>
2025-11-09 13:10:21 +0000

    Starting the project, first commit

---
+++ CrossPlatformProject/CrossPlatformProject/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿//This is the first commit, This is the initial launch of the github page
+//Daniel Nelga's Project, G00464658
+namespace CrossPlatformProject
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
